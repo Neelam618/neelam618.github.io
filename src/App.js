79ef21c664b24cd6c95f5def0702700ac83c1e1e
Neelam Chavan <neelamchavan410@gmail.com>
2023-04-08 18:29:32 +0530

    refactor home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Navbar from './components/Navbar';
 import { purple } from '@mui/material/colors';
 import Home from './components/home/Home';
-import Waves from "./images/wave.svg";
-import WavesMobile from "./images/wave2.svg"
-import { Box } from '@mui/material';
-import Skills from './components/Skills';
+
 
 const theme = createTheme({
   palette: {
@@ -27,11 +23,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ backgroundImage: { xs: `url(${WavesMobile})`, md: `url(${Waves})` }, backgroundRepeat: "no-repeat", padding: { xs: '1em 0', sm: '1.5em 0', md: '2.5em 1em', lg: '4em 1em' } }}>
-        <Navbar />
-        <Home />
-      </Box>
-      <Skills />
+      <Home />
     </ThemeProvider>
   );
 }
