208ce8638cad5b694b7cd4924f4291f10848ec8e
Neelam Chavan <neelamchavan410@gmail.com>
2023-04-13 12:01:22 +0530

    refactor skills and add favicon

--- src/components/home/Skills.jsx
+++ src/components/home/Skills.jsx
@@ -2,18 +2,22 @@ import { Box, Chip, Container, Typography } from '@mui/material'
 import React from 'react'
 import Waves from "../../images/skillsbg.svg"
 
+const skills = ["React", "JavaScript", "Next.js", "Typescript", "HTML", "CSS", "Bootstrap", "jQuery", "SASS", "Git", "Tailwind CSS", "Material UI"]
+
 function Skills() {
     return (
-        <Box sx={{ backgroundImage: `url(${Waves})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", padding: { xs: '1em 0', sm: '1.5em 0', md: '2.5em 1em', lg: '4em 1em', marginBottom: "6em", height: "300px" } }}>
+        <Box sx={{ backgroundImage: `url(${Waves})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", padding: { xs: '1em 0', sm: '1.5em 0', md: '1em 1em', marginBottom: "6em", height: "300px" } }}>
             <Container >
                 <Typography variant="h5" component="h3">Skills</Typography>
                 <Box sx={{ background: "#fff", borderRadius: "10px", padding: { xs: "1em", md: "2em 4em" }, margin: "1em 0" }}>
                     <Box sx={{ display: 'flex', gap: "20px", flexWrap: "wrap" }}>
-                        <Chip label="React" color="primary" />
-                        <Chip label="JavaScript" color="primary" />
-                        <Chip label="HTML" color="primary" />
-                        <Chip label="CSS" color="primary" />
-                        <Chip label="Typescript" color="primary" />
+                        {
+                            skills.map(skill => {
+                                return (
+                                    <Chip label={skill} color="primary" />
+                                )
+                            })
+                        }
                     </Box>
                 </Box>
             </Container>
